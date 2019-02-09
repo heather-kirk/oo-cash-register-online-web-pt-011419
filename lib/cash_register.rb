7225030312457cf64957f017663a7cb01d702baa
Heather Kirk <hskirk@gmail.com>
2019-02-09 19:18:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
   @total += price * quantity 
   quantity.times do 
-  @items << title.times
+  @items << title
   end 
 end 
   def apply_discount
